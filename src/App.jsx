ce0ab7e1093b9daaede646e9168fc66d2fad1ec7
Rcoder-singh <rishabh22.singh@gmail.com>
2024-02-03 00:34:35 +0530

    Feature Section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Provider } from "react-redux";
 import { store } from "./redux/store";
 import Navbar from "./Components/Navbar";
 import Hero from "./Components/Hero";
+import Feature from "./Components/Feature";
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Provider store={store}>
         <Navbar />
         <Hero />
+        <Feature />
       </Provider>
     </div>
   );
