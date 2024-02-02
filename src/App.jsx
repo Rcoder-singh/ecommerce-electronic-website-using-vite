78ed137f4802f8f5671893d527b193c4f1324645
Rishabh Singh <rishabh22.singh@gmail.com>
2024-02-02 23:07:00 +0530

    Hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
 import Navbar from "./Components/Navbar";
+import Hero from "./Components/Hero";
 
 const App = () => {
   return (
     <div>
       <Provider store={store}>
         <Navbar />
+        <Hero />
       </Provider>
     </div>
   );
