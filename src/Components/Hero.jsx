2e54936334fdf8ad8a230b06c6110548cf18d93c
Rishabh Singh <rishabh22.singh@gmail.com>
2024-02-03 00:29:37 +0530

    Font error fixed

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -13,7 +13,7 @@ const Hero = () => {
               {" "}
               The best note book collection 2023
             </h2>
-            <h3 className="text-2xl font-Oregano">
+            <h3 className="text-2xl font-[Oregano]">
               Exclusive offer <span className="text-red-600">-10%</span> off
               this week
             </h3>
